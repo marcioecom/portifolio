986b481b6d98209808e47de05a8c7f1cdda07e9e
Marcio Junior <marcio.mj70@gmail.com>
2022-05-26 10:29:44 -0300

    feat: add chakra ui and clean project

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Heading } from '@chakra-ui/layout'
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <Heading>Hello, World!</Heading>
+  )
+}
+
+export default Home
